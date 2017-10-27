354f6156f035fccdf9deb2102cd36e758674ecb6
kelmory06 <32686424+kelmory06@users.noreply.github.com>
2017-10-27 18:38:24 -0500

    Update Descargas.py

--- Descargas.py
+++ Descargas.py
@@ -1,4 +1,4 @@
-En el  modulo descargar ya se debe abrir la conexion entre la intranet y  la pasare ,
-en esta se consultara periodicamente si  hay descargas pendientes, si estas existen  se debe enviar
-la informacion necesaria  para iniciar la descaga  (la informacion se envia a transmission) y poder monitorearla,
-en caso d eque no exista sigue con la consulta.
+En el  modulo descarga ya se debe abrir la conexion entre la intranet y  la pasarela ,
+en este se consultara periodicamente si  hay descargas pendientes, si estas existen  se debe enviar
+la informacion necesaria  para iniciar o continuar la descarga  (la informacion se envia a transmission)
+y poder monitorearla, en caso d eque no exista sigue con la consulta.
